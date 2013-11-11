cb7de205a7b8d5c88b2f8103073f6752728357c6
Gregory McIntyre <greg@gregorymcintyre.com>
2013-11-12 01:26:57 +1100

    Enable livereload plugin.

--- config.rb
+++ config.rb
@@ -1,13 +1,17 @@
 require 'lib/helpers/custom_helpers'
+
 helpers CustomHelpers
 
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-activate :directory_indexes
+ignore '/courses/_template*'
 
-# Markdown
+activate :directory_indexes
+activate :livereload
+activate :rouge_syntax # Code highlighting
+
 set :markdown_engine, :redcarpet
 set :markdown, {
   :fenced_code_blocks => true,
@@ -16,13 +20,7 @@ set :markdown, {
   :autolink => true,
 }
 
-ignore '/courses/_template*'
-
-# Code highlighting
-activate :rouge_syntax
-
 configure :build do
-  # Allow deployment to a subdirectory such as /microcourses/
   activate :relative_assets
   set :relative_links, true
 end
