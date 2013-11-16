0edc98270d4c9c2251cf1303d9ec103fd737a515
Joshua Paling <joshua.paling@gmail.com>
2013-11-16 23:51:15 +1100

    break into pages and add top nav
    Signed-off-by: Joshua Paling <joshua.paling@gmail.com>

--- config.rb
+++ config.rb
@@ -23,3 +23,11 @@ configure :build do
   activate :relative_assets
   set :relative_links, true
 end
+
+helpers do
+  def nav_link(link_text, url, options = {})
+    options[:class] ||= ""
+    options[:class] << " active" if url == current_page.url
+    link_to(link_text, url, options)
+  end
+end
